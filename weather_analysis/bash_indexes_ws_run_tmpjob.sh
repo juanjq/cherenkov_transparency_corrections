27ba31e802916cd4d742cf2f60012f5f5debe300
juanjq <juanjimenezquiles34@gmail.com>
2024-01-23 16:06:24 +0000

    +

--- weather_analysis/bash_indexes_ws_run_tmpjob.sh
+++ weather_analysis/bash_indexes_ws_run_tmpjob.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /fefs/aswg/workspace/juan.jimenez/cherenkov_transparency_corrections/weather_analysis/script_relation_ws_run.py '744000,749999'
+python /fefs/aswg/workspace/juan.jimenez/cherenkov_transparency_corrections/weather_analysis/script_relation_ws_run.py '750000,755999'
