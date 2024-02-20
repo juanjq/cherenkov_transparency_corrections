3e1dbf8b13a61ec4ab3f413c98806f1ae662ea56
juanjq <juanjimenezquiles34@gmail.com>
2024-02-20 08:53:39 +0000

    Notebooks for srunwise

---
+++ data_processing_srunwise/script_merge.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys
+def print_test():
+    print(f"test_merge")
+
+if __name__ == "__main__":
+    print_test()
---
+++ data_processing_srunwise/script_scale_init.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+def print_test(text):
+    print(f"test_init - {text}")
+
+if __name__ == "__main__":
+    t = sys.argv[1]
+    print_test(t)
