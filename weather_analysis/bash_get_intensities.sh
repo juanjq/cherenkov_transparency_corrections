55971587275a6353736f6e7db42f98173a88f5eb
juanjq <juanjimenezquiles34@gmail.com>
2024-05-14 18:44:17 +0000

    Updated weather_analysis

---
+++ weather_analysis/bash_get_intensities.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Store the command line arguments in a variable
+string_argument="$1"
+
+# Call your Python script and pass the argument
+python script_get_intensities.py "$string_argument"
+
--- weather_analysis/bash_indexes_ws_run_tmpjob.sh
+++ weather_analysis/bash_indexes_ws_run_tmpjob.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /fefs/aswg/workspace/juan.jimenez/cherenkov_transparency_corrections/weather_analysis/script_relation_ws_run.py '750000,755999'
+python /fefs/aswg/workspace/juan.jimenez/cherenkov_transparency_corrections/weather_analysis/script_relation_ws_run.py '768000,773999'
