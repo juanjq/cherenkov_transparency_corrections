bc750fc69e072b822b934a2fc123da27d6dc9407
juanjq <juanjimenezquiles34@gmail.com>
2024-05-22 07:42:55 +0000

    Added total plots

--- weather_analysis/bash_get_intensities.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Store the command line arguments in a variable
-string_argument="$1"
-
-# Call your Python script and pass the argument
-python script_get_intensities.py "$string_argument"
-
